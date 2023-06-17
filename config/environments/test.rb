0d3e04e10c7bbf75ec6bfc2f185a5db6dd6b3e4d
Hien Nguyen <quochien@gmail.com>
2023-06-17 09:54:09 +0700

    Github action CI: wip

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,6 +8,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
   # Turn false under Spring and add config.action_view.cache_template_loading = true.
   config.cache_classes = true
 
